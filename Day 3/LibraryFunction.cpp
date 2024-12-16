b40053a736a8910b8f7f2d9aa5955b610dda70f8
Priyam Dutta <priyamdutta624@gmail.com>
2024-12-16 17:58:11 +0530

    code updated

--- Day 3/LibraryFunction.cpp
+++ Day 3/LibraryFunction.cpp
@@ -1,1 +1,37 @@
-// continued...
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void reverseArray(vector<int> &arr)
+{
+    reverse(arr.begin(), arr.end());
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size: ";
+    cin >> n;
+
+    vector<int> v;
+
+    for (int i = 0; i < n; i++)
+    {
+        int value;
+        cin >> value;
+        v.push_back(value);
+    }
+
+    reverseArray(v);
+
+    cout << "The Reversed array is: ";
+
+    for (int i : v)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
